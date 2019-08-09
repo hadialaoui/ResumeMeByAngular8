5525e54079e7eb5a6c96c43471f562ac1572fac3
Abdelhakim Hadi Alaoui <abdelhakim.hadi.alaoui@ibm.com>
2019-08-09 17:02:54 +0100

    activate route commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CertificationsListComponent } from './certifications-list/certifications-list.component';
+import { DetailCertifComponent } from './detail-certif/detail-certif.component';
 
 
 
 const routes: Routes = [
-  {path: 'certifications', component: CertificationsListComponent}
+  {path: 'certifications', component: CertificationsListComponent},
+  {path: 'certifications/detail/:idCertif', component: DetailCertifComponent},
 ];
 
 @NgModule({
--- src/app/detail-certif/detail-certif.component.ts
+++ src/app/detail-certif/detail-certif.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { certifications } from '../certifications';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detail-certif',
@@ -7,10 +8,19 @@ import { certifications } from '../certifications';
   styleUrls: ['./detail-certif.component.css']
 })
 export class DetailCertifComponent implements OnInit {
- @Input() certification;
-  constructor() { }
+  certification;
+ /*@Input() certification;*/
+  constructor(private route: ActivatedRoute) { 
+    
+  }
 
   ngOnInit() {
+    this.route.paramMap.subscribe( params => {
+      this.certification = certifications[+params.get('CertifId')];
+    }
+
+    )
+    
   }
 
 }
